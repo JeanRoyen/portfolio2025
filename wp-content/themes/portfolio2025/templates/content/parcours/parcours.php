8f34d90d91f2a5267aee9b8fb266034567b28fae
JeanRoyen <jean.royen@student.hepl.be>
2025-05-03 20:48:51 +0200

    Added every commits below for style

---
+++ wp-content/themes/portfolio2025/templates/content/parcours/parcours.php
@@ -0,0 +1,20 @@
+<?php $headline = get_sub_field('headline') ?>
+<?php $experiences = get_sub_field('experiences') ?>
+<?php $class = get_sub_field('cs_class') ?>
+
+<section class="">
+    <h2 class="section_container_title"><?= $headline ?></h2>
+    <div class="parcours__grid <?= $class !== '' ? $class : '' ?>">
+        <?php foreach ($experiences as $ex): ?>
+            <article class="parcours">
+                <h3 class="div_project_h2"><?= $ex['title'] ?></h3>
+                <div class="parcours_card">
+                    <?= $ex['number'] ?>
+                </div>
+                <div class="parcours_description">
+                    <?= $ex['description'] ?>
+                </div>
+            </article>
+        <?php endforeach; ?>
+    </div>
+</section>
