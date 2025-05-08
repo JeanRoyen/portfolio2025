dd8bbba1d3941957d0d1959e4d30f0e5ea059efe
JeanRoyen <jean.royen@student.hepl.be>
2025-05-08 13:16:40 +0200

    Added parcours style

--- wp-content/themes/portfolio2025/templates/content/parcours/parcours.php
+++ wp-content/themes/portfolio2025/templates/content/parcours/parcours.php
@@ -1,6 +1,6 @@
 <?php $headline = get_sub_field('headline') ?>
 <?php $experiences = get_sub_field('experiences') ?>
-<?php $class = get_sub_field('cs_class') ?>
+<?php $class = get_sub_field('class') ?>
 
 <section class="">
     <h2 class="section_container_title"><?= $headline ?></h2>
